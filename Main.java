51a7f9f32fb1c2728bd100c9a370c18eb0e8fd90
Sourav <sourav10singh11@gmail.com>
2023-05-11 14:21:15 +0530

    Encapsulation Implementation

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        RWOnly r = new RWOnly();
+
+        r.setName("Sourav");
+
+        System.out.println(r.getName());
+    }
+}
