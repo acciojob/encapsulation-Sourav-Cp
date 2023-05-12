f074b3fcd69f187cdf38a8186c47a885eb6ab455
Sourav <sourav10singh11@gmail.com>
2023-05-12 11:58:39 +0530

    Final Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String args[])
+    {
+
+        RWOnly rwOnly = new RWOnly();
+        rwOnly.setName("Sourav");
+        String name = rwOnly.getName();
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
+
+    private String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
     
 }
